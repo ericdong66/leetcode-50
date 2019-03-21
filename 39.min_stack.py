22272fe780ccd22bc4d320627dab5fd1075a39a5
Eric Dong <ericdong66@gmail.com>
2019-03-20 17:09:18 -0700

    mini stack re impl

--- 39.min_stack.py
+++ 39.min_stack.py
@@ -10,39 +10,65 @@
 # Space: O(1)
 
 
+class Item(object):
+    def __init__(self, value, counter=1):
+        self.value = value
+        self.counter = counter
+
+    def __repr__(self):
+        return "{}.{}".format(self.value, self.counter)
+
+
+class Min(object):
+    def __init__(self):
+        self.mini_stack = list()
+
+    def push(self, x):
+        if len(self.mini_stack):
+            top = self.mini_stack[-1]
+            if x < top.value:
+                self.mini_stack.append(Item(x))
+            elif x == top.value:
+                top.counter += 1
+        else:
+            self.mini_stack.append(Item(x))
+
+    def pop(self, x):
+        top = self.mini_stack[-1]
+        if x == top.value:
+            top.counter -= 1
+            if top.counter == 0:
+                self.mini_stack.pop()
+
+    def top(self):
+        return self.mini_stack[-1].value
+
+
 class MinStack(object):
 
     def __init__(self):
-        self.stack, self.minStack = [], []
+        self.stack = []
+        self.mini_stack = Min()
 
     def push(self, x):
         self.stack.append(x)
-        if len(self.minStack):
-            if x < self.minStack[-1][0]:
-                self.minStack.append([x, 1])
-            elif x == self.minStack[-1][0]:
-                self.minStack[-1][1] += 1
-        else:
-            self.minStack.append([x, 1])
+        self.mini_stack.push(x)
 
     def pop(self):
         x = self.stack.pop()
-        if x == self.minStack[-1][0]:
-            self.minStack[-1][1] -= 1
-            if self.minStack[-1][1] == 0:
-                self.minStack.pop()
+        self.mini_stack.pop(x)
 
     def top(self):
         return self.stack[-1]
 
     def get_min(self):
-        return self.minStack[-1][0]
+        return self.mini_stack.top()
 
 
 if __name__ == "__main__":
     stack = MinStack()
-    for i in [1, 2, 3, 3, 1]:
+    for i in [2, 2, 3, 2, 3, 1, 1, 1, 5]:
         stack.push(i)
     while stack.stack:
-        print(stack.top(), stack.get_min())
+        print(stack.top(), stack.get_min(), stack.mini_stack.mini_stack[-1])
         stack.pop()
