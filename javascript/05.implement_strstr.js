be6391fe679a8ec739086ab21f6116e48ab2a46b
Eric Dong <ericdong66@gmail.com>
2020-01-05 22:00:20 -0800

    strstr js solution

---
+++ javascript/05.implement_strstr.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    for (var i = 0; i < haystack.length - needle.length +1; i++) {
+        if (haystack.slice(i, needle.length + i) === needle) return i;
+    }
+    return -1
+};
